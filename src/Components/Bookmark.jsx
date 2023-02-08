fcd1fa6976d42066bef7f79f2c87ee4ffafbaca8
Ben King <benking1293@gmail.com>
2023-02-08 21:22:21 +0000

    created bookmark component

--- src/Components/Bookmark.jsx
+++ src/Components/Bookmark.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
-export default function Bookmark() {
+export default function Bookmark({ link }) {
   return (
-    <div>Bookmark</div>
+    <>
+        <div className='link-container'>
+            <div className='link-info-container'>
+            {/* will contain favicon img */}
+            <img />
+            {/* bookmark title wrapped in a link tag */}
+                <a href={link.url} target='_blank'>
+                <div>{link.title}</div>
+                </a>
+            </div>
+            
+            {/* edit and delete buttons */}
+            <div className='edit-delete-container'>
+                <button 
+                className='btn link edit'>
+                    Edit
+                </button>
+                <button 
+                className='btn link trash'>
+                    trash
+                </button>
+            </div>
+        </div>
+    </>
   )
 }
